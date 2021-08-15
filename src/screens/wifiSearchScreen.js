00b899779ac68f985cd2f1816b2dc499bcb168c2
pedro <pedrosingularyhc@gmail.com>
2021-08-15 08:54:07 -0300

    V0.0.2
    wifi support added

--- src/screens/wifiSearchScreen.js
+++ src/screens/wifiSearchScreen.js
@@ -1,25 +1,31 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, } from "react";
 import {
     View,
+    Text,
     TextInput,
     Button,
     TouchableOpacity,
     SafeAreaView,
     ScrollView,
-    Text,
     NetInfo,
     Platform,
     Alert,
     ToastAndroid,
+    StyleSheet,
+    DeviceEventEmitter,
 } from "react-native";
-
+import FindLocalDevices from 'react-native-find-local-devices';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const defaultPort = 81;
 const numericRegex = /[^0-9]/g;
 
 const WifiSearchScreen = () => {
-    const [devices, deviceState] = useState([]);
+    const [devices, devicesState] = useState([]);
+    const [device, deviceState] = useState(undefined);
+    const [searchButton, searchButtonState] = useState(false);
+    const [searching, searchingState] = useState(false);
     const [port, portState] = useState(defaultPort);
 
     var id = 0;
@@ -31,44 +37,105 @@ const WifiSearchScreen = () => {
 
         if (port > 65535 || port < 1)
             return portState(defaultPort);
-        return portState(parseInt(newPort.replace(regex, "")));
+        return portState(parseInt(port));
     };
     const search = () => {
         if (!port) return;
 
+        devicesState([]);
+        searchingState(true);
+        searchButtonState(true);
 
+        DeviceEventEmitter.addListener('NEW_DEVICE_FOUND', (device) => {
+            devicesState([...devices, `${device.ipAddress}:${device.port}`])
+        });
+
+        DeviceEventEmitter.addListener('CHECK', (device) => {
+            deviceState(`${device.ipAddress}:${device.port}`);
+        });
+
+        DeviceEventEmitter.addListener('RESULTS', () => {
+            ToastAndroid.showWithGravityAndOffset("Busca Completa!", ToastAndroid.SHORT, ToastAndroid.BOTTOM, 0, 80);
+            searchingState(false);
+            deviceState(undefined);
+            searchButtonState(false);
+        });
+
+        // DeviceEventEmitter.addListener('NO_DEVICES', () => {});
+
+        // DeviceEventEmitter.addListener('NO_PORTS', () => {  });
+
+        // DeviceEventEmitter.addListener('CONNECTION_ERROR', (error) => { console.log(error.message); });
+
+        // FindLocalDevices.cancelDiscovering();
+
+        setTimeout(() => FindLocalDevices.getLocalDevices({
+            ports: [port],
+            timeout: 40
+        }), 100);
     };
-    const device = (ip) => {
-        return (<TouchableOpacity key={id++}>
-            <Text>{ip}</Text>
+    const deviceList = (ip) => {
+        return (<TouchableOpacity key={id++} style={{ margin: 0, marginLeft: 5, }} onPress={() => deviceOnPress(ip)}>
+            <Text style={{ fontSize: 25, }}>{ip}</Text>
         </TouchableOpacity>);
     };
-    return (<View>
-        <SafeAreaView style={{ width: 300, height: "100%",}}>
-            {devices.length > 0 ? <ScrollView style={{ width: 300, height: 300 }}>
-                {devices.map((i) => device(i))}
-            </ScrollView> : <View style={{
-                width: 300,
-                height: 300,
-                backgroundColor: "#666666",
-                justifyContent: "center",
-                alignItems: "center",
-                alignContent: "center",
-            }}>
-                <Text  >Nenhum Dispositivo Detectado!</Text>
+    const deviceOnPress = async (ip) => {
+        if (!ip)
+            return ToastAndroid.showWithGravityAndOffset("ERRO", ToastAndroid.SHORT, ToastAndroid.BOTTOM, 0, 80);
+
+        await AsyncStorage.setItem("connection", { connectionType: 0, ip });
+    };
+
+    return (<View style={styles.container}>
+        <SafeAreaView style={{ ...styles.devicesContainer, }}>
+            {searching ? <View style={styles.noDevices}>
+                <Text>Procurando dispositivos...</Text>
+                <Text>{device}</Text>
+            </View> : devices.length > 0 ? <ScrollView style={{ width: 300, height: 300 }}>
+                {devices.map((i) => deviceList(i))}
+            </ScrollView> : <View style={styles.noDevices}>
+                <Text>Nenhum Dispositivo Detectado!</Text>
             </View>}
         </SafeAreaView>
 
         <View>
             <Text>Porta: </Text>
-            <TextInput placeholder="Porta" keyboardType='numeric' onChangeText={changePort} value={port ? String(port) : undefined} />
+            <TextInput
+                placeholder="Porta"
+                keyboardType='numeric'
+                onChangeText={changePort}
+                value={port ? String(port) : undefined}
+            />
             <Button
-                title="Procurar"
-                onPress={() => ToastAndroid.showWithGravityAndOffset("Em manutenção!", ToastAndroid.SHORT, ToastAndroid.BOTTOM, 0, 80)}
-                disabled={false}
+                title={searchButton ? "Procurando" : "Procurar"}
+                onPress={search}
+                disabled={searchButton}
             />
         </View>
     </View>);
 };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column"
+    },
+    devicesContainer: {
+        width: 300,
+        height: 300,
+        borderColor: "#000000",
+        borderWidth: 5,
+        borderRadius: 15,
+        overflow: "hidden",
+        margin: 5,
+    },
+    noDevices: {
+        width: 300,
+        height: 300,
+        justifyContent: "center",
+        alignItems: "center",
+        alignContent: "center",
+    },
+});
 
 export default WifiSearchScreen;
